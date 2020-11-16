7a8c8266ef72f3d0871196ef382d9b1129be821a
nath-btx <nathan.bertiaux@hotmail.fr>
2020-11-16 11:42:20 +0100

    e02 finis

--- onecode/season.03/e01.js
+++ onecode/season.03/e01.js
@@ -11,10 +11,11 @@ if (!port) {
 
 
 // execute HTTP server
-const server = http.createServer ( (req, res) => {
-   res.setHeader("Content-Type", "text/html");
-   res.write('<h1> Hello World <h1>');
-   res.end;
+const server = http.createServer ((req, res) => {
+
+  res.setHeader("Content-Type", "text/html");
+  res.write('<h1> Hello World <h1>');
+  res.end;
 })
 
 
